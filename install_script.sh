47b6dd5726d862d129a4625a2d1ecfd06b71116a
Oleksandr Tkachov <131679190+tkachovua@users.noreply.github.com>
2023-06-30 19:01:11 +0300

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -sSL -O pre-commit https://raw.githubusercontent.com/tkachovua/devsecops/main/pre-commit-midle 
+curl -sSL -o pre-commit https://raw.githubusercontent.com/tkachovua/devsecops/main/pre-commit-midle 
 chmod +x pre-commit
 export PATH="~/.gitleaks/bin:$PATH"
